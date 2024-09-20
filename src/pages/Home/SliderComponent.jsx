bd5333b7863b2212fc61427511d29b9cc2a07765
Sajjad-Hosan <s4jj4dho34n@hmail.com>
2024-09-20 13:09:22 +0600

    home page ui customize, new style libary added, slider card added

--- src/pages/Home/SliderComponent.jsx
+++ src/pages/Home/SliderComponent.jsx
@@ -1,65 +1,50 @@
 // SliderComponent.jsx
-import Slider from 'react-slick'; // Import react-slick slider
-import 'slick-carousel/slick/slick.css'; // Slick carousel CSS
-import 'slick-carousel/slick/slick-theme.css'; // Slick carousel theme
+import Slider from "react-slick"; // Import react-slick slider
+import "slick-carousel/slick/slick.css"; // Slick carousel CSS
+import "slick-carousel/slick/slick-theme.css"; // Slick carousel theme
+import { FaCartShopping } from "react-icons/fa6";
+
+const SliderCard = ({ data }) => {
+  return (
+    <>
+      <div className="w-full max-h-96 relative card overflow-hidden pl-3">
+        <img
+          src="/src/assets/book3.jpg"
+          alt="Slide 3"
+          className="object-cover rounded-lg"
+        />
+        <div className="absolute bottom-0 left-4 text-left bg-transparent bg-opacity-30 w-full p-2 overflow-hidden">
+          <h1 className="text-lg font-semibold text-black mt-2">Book Name</h1>
+          <div className="mt-4">
+            <button className="btn btn-sm btn-ghost text-black">
+              <FaCartShopping className="text-lg" /> Shop
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 const SliderComponent = () => {
   // Slider settings for slick
   const sliderSettings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    speed: 300,
+    slidesToShow: 2,
+    centerMode: true,
+    variableWidth: true,
     autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false, // Disable navigation arrows
+    autoplaySpeed: 2000,
   };
 
   return (
     <div className="container mx-auto my-8">
       <Slider {...sliderSettings}>
-        {/* Slide 1 */}
-        <div className="relative">
-          <img
-            src="/src/assets/book1.jpg"
-            alt="Slide 1"
-            className="w-full max-h-96"
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <button className="bg-red-500 text-white px-6 py-3 rounded-full text-lg hover:bg-red-600 transition duration-300">
-              Shop Now
-            </button>
-          </div>
-        </div>
-
-        {/* Slide 2 */}
-        <div className="relative">
-          <img
-            src="/src/assets/book2.jpg"
-            alt="Slide 2"
-            className="w-full max-h-96"
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <button className="bg-red-500 text-white px-6 py-3 rounded-full text-lg hover:bg-red-600 transition duration-300">
-              Shop Now
-            </button>
-          </div>
-        </div>
-
-        {/* Slide 3 */}
-        <div className="relative">
-          <img
-            src="/src/assets/book3.jpg"
-            alt="Slide 3"
-            className="w-full max-h-96"
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <button className="bg-red-500 text-white px-6 py-3 rounded-full text-lg hover:bg-red-600 transition duration-300">
-              Shop Now
-            </button>
-          </div>
-        </div>
+        {[1, 2].map((i) => (
+          <SliderCard key={i} data={i} />
+        ))}
       </Slider>
     </div>
   );
