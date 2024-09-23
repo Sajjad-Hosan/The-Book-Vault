402cbd15baa58bed3891c6389193e08fd43aadab
Your NameIfranAlamBishal <you@example.comifranalam4089@gmail.com>
2024-09-24 04:23:45 +0600

    update: updated local database.

--- src/pages/ProductsPage/ProductCard.jsx
+++ src/pages/ProductsPage/ProductCard.jsx
@@ -3,17 +3,17 @@ import { IoStarHalfOutline } from "react-icons/io5";
 const ProductCard = ({ book }) => {
     const { title, author, cover_pic, genre, price, rating } = book;
     return (
-        <div className="hero bg-slate-100 ">
-            <div className="hero-content flex-col lg:flex-row">
+        <div className="hero bg-slate-100 rounded-lg">
+            <div className="hero-content flex-col lg:flex-row gap-5 py-7">
                 <img
                     src={cover_pic}
                     className=" w-[200px] h-[300px] rounded-lg shadow-2xl" />
-                <div className=" space-y-2">
+                <div className=" space-y-2 w-[200px]">
                     <h1 className="text-2xl font-semibold">{title}</h1>
-                    <p><span className=" font-semibold">By:</span> {author}</p>
+                    <p className=" text-lg"><span className=" font-semibold">By:</span> {author}</p>
                     <small className=" font-semibold">{genre}</small>
                     <p>{rating}<IoStarHalfOutline className=" inline pb-1"/></p>
-                    <p className="py-2 font-bold">
+                    <p className="py-2 font-bold text-xl">
                         {price} BDT
                     </p>
                     <button className="btn btn-primary">View Details</button>
