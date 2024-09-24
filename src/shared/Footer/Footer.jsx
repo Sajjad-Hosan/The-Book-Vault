cbc8e3dd710ff8fa3a2d531e223e0c71f53cc198
Anik98B <anikbhowmik0188@gmail.com>
2024-09-25 02:30:25 +0600

    added a footer design

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -2,7 +2,11 @@
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
+            <footer className="footer footer-center bg-base-300 text-base-content p-4">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by The-Book-Vault Ltd</p>
+                </aside>
+            </footer>
         </div>
     );
 };
