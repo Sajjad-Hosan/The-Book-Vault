3071513ff0c52e25c10b01925e09342fe6ad8f37
Anik98B <anikbhowmik0188@gmail.com>
2024-09-24 23:37:16 +0600

    added a footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./shared/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Outlet />
+      <Footer></Footer>
     </>
   );
 };
