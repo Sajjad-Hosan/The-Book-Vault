9659da1c6b39af3cfbb01d8feec4d9253a0449db
Your NameIfranAlamBishal <ifranalam4089@gmail.com>
2024-10-14 14:08:00 +0600

    add: Add to wishlist Feature added on product details page.

---
+++ src/Hooks/useAxios.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const useAxios = () => {
+
+    const axiosSecure = axios.create({
+        baseURL: 'https://the-book-vault-backend.vercel.app'
+        // baseURL: 'http://localhost:5000'
+    })
+
+    return axiosSecure;
+};
+
+export default useAxios;
